dda374588ec1c3f114a19b425dcaee711b7ddd3b
Marco <mi4998@scaryblankplanet>
2020-09-08 10:22:07 +0200

    swapped skeleton and  updated Live files

--- live/live.c
+++ live/live.c
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2015 Cisco and/or its affiliates.
+ * live.c - skeleton vpp engine plug-in
+ *
+ * Copyright (c) <current-year> <your-organization>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -12,175 +14,52 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**      
- * @file
- * @brief SRv6 Live-Live Plugin, plugin API / trace / CLI handling.
- */
 
 #include <vnet/vnet.h>
 #include <vnet/plugin/plugin.h>
-#include <vnet/fib/ip6_fib.h>
 #include <live/live.h>
-#include <live/live_replicate_dpo.h>
 
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
+#include <vpp/app/version.h>
+#include <stdbool.h>
 
-/* define message IDs */
-#include <live/live_msg_enum.h>
+#include <live/live.api_enum.h>
+#include <live/live.api_types.h>
 
-/* define message structures */
-#define vl_typedefs
-#include <live/live_all_api_h.h> 
-#undef vl_typedefs
-
-/* define generated endian-swappers */
-#define vl_endianfun
-#include <live/live_all_api_h.h> 
-#undef vl_endianfun
-
-/* instantiate all the print functions we know about */
-#define vl_print(handle, ...) vlib_cli_output (handle, __VA_ARGS__)
-#define vl_printfun
-#include <live/live_all_api_h.h> 
-#undef vl_printfun
-
-/* Get the API version number */
-#define vl_api_version(n,v) static u32 api_version=(v);
-#include <live/live_all_api_h.h>
-#undef vl_api_version
-
-#define REPLY_MSG_ID_BASE sm->msg_id_base
+#define REPLY_MSG_ID_BASE lmp->msg_id_base
 #include <vlibapi/api_helper_macros.h>
-
-/* List of message types that this plugin understands */
-
-#define foreach_live_plugin_api_msg                           \
-_(LIVE_ENABLE_DISABLE, live_enable_disable)
-
-/* *INDENT-OFF* */
-VLIB_PLUGIN_REGISTER () = {
-    .version = LIVE_PLUGIN_BUILD_VER,
-    .description = "SRv6 Live-Live Policy Plugin",
-};
-/* *INDENT-ON* */
 
 live_main_t live_main;
 
-static dpo_type_t sr_live_pr_encaps_dpo_type;
+/* Action function shared between message handler and debug CLI */
 
-/**
- * @brief live-live rewrite computation  
- *
- * Function to modify the rewrite parameters computed 
- * for the SRv6 Encapsulation Policy
- */
-
-static inline u8 *
-live_compute_rewrite_encaps (u8 * rs)
-{
- 
-  ip6_header_t *iph;
-  ip6_sr_header_t *srh;
-  live_tlv_t *live_tlv;
-
-  /* Resize precompute vector */ 
-  vec_resize (rs, sizeof(live_tlv_t));
-  
-  /* Adding live-live tlv size to IPv6 payload length field */
-  iph = (ip6_header_t *)rs;
-  iph->payload_length += sizeof(live_tlv_t);
-
-  /* Adding live-live tlv size in octets to SRH length field 
-   * live-live tlv size: 1 byte */
-  srh = (ip6_sr_header_t *)(iph + 1);
-  srh->length += sizeof(live_tlv_t)/8;
-
-  /* Attach the TLV to the end of SL */
-  live_tlv = (live_tlv_t *)(&srh->segments[srh->segments_left] +1);
-
-  /* Type and Length of TLV accordingly to SRH TLV data structure. 
-   * Flow ID and Sequence Number initialized to 0 */
-  live_tlv-> type =2;
-  live_tlv-> length =7;
-  live_tlv-> flow =0;
-  live_tlv-> seqnum =0;
-  return rs;
-}
-
-
-int live_enable_disable (live_main_t * sm, ip6_address_t * bsid,
+int live_enable_disable (live_main_t * lmp, u32 sw_if_index,
                                    int enable_disable)
 {
-  ip6_sr_main_t * srm = &sr_main;
-   int rv = 0;
-  ip6_sr_policy_t *policy;
-  uword *p=0;
-  u32 *sl_index;
-  ip6_sr_sl_t *segment_list;
-  load_balance_path_t path;
-  path.path_index = FIB_NODE_INDEX_INVALID;
-  load_balance_path_t *ip6_path_vector =0;
+  vnet_sw_interface_t * sw;
+  int rv = 0;
 
-  p = mhash_get (&srm->sr_policies_index_hash, bsid);
+  /* Utterly wrong? */
+  if (pool_is_free_index (lmp->vnet_main->interface_main.sw_interfaces,
+                          sw_if_index))
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
 
-  if(p==0)
-	{ 
-		rv = 1;
- 		return rv;
-	}
-  
-  /* Spray policy implementation with Live-Live features :
-   * 1. Policy DPO to the Live-Live replication node
-   * 2. SLs DPOs to the SRv6 Live-Live encapsulation node
-  */
+  /* Not a physical port? */
+  sw = vnet_get_sw_interface (lmp->vnet_main, sw_if_index);
+  if (sw->type != VNET_SW_INTERFACE_TYPE_HARDWARE)
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
 
-  /* Pointer to the existing policy that need to be Live-Live confiured */
-  policy = pool_elt_at_index(srm->sr_policies, p[0]);
-  /* Setting the live-live DPO */
-  dpo_reset(&policy->ip6_dpo);
-  live_policy_dpo_set (&policy->ip6_dpo, DPO_PROTO_IP6, policy - srm->sr_policies);
+  live_create_periodic_process (lmp);
 
-      fib_prefix_t pfx = {
-  .fp_proto = FIB_PROTOCOL_IP6,
-  .fp_len = 128,
-  .fp_addr = {
-        .ip6 = policy->bsid,
-        }
-      };
+  vnet_feature_enable_disable ("device-input", "live",
+                               sw_if_index, enable_disable, 0, 0);
 
-  /* Remove existing spray policy DPO from the FIB */  
-  fib_table_entry_special_remove (srm->fib_table_ip6, &pfx, FIB_SOURCE_SR);
-  /* Update FIB entry's DPO pointing the Live-Live processing node */
-  fib_table_entry_special_dpo_update (srm->fib_table_ip6,
-            &pfx,
-            FIB_SOURCE_SR,
-            FIB_ENTRY_FLAG_EXCLUSIVE,
-            &policy->ip6_dpo);
-
-
-  path.path_weight =1;
-  vec_foreach (sl_index, policy->segments_lists)
-  {
-    segment_list =  pool_elt_at_index(srm->sid_lists, *sl_index);
-    /* Modify the precompute size to Encap */
-    segment_list->rewrite = live_compute_rewrite_encaps(segment_list->rewrite); 
-    /* Change the SLs' DPO to the Live-Live encapsulation DPO */
-    dpo_reset(&segment_list->ip6_dpo);
-    dpo_set (&segment_list->ip6_dpo, sr_live_pr_encaps_dpo_type, DPO_PROTO_IP6,
-         segment_list - srm->sid_lists);
-    path.path_dpo = segment_list->ip6_dpo;
-    vec_add1(ip6_path_vector, path);
-  }
-
-  /* Update Live-Live replicate multipath */
-  live_replicate_multipath_update(&policy->ip6_dpo, ip6_path_vector);
-
-  /* Setting the Live-Live policy type*/
-  policy->type = SR_POLICY_TYPE_LIVE;
-  /* Keep modified policy in the plugin*/
-  sm->live_policy = policy;
-
+  /* Send an event to enable/disable the periodic scanner process */
+  vlib_process_signal_event (lmp->vlib_main,
+                             lmp->periodic_node_index,
+                             LIVE_EVENT_PERIODIC_ENABLE_DISABLE,
+                            (uword)enable_disable);
   return rv;
 }
 
@@ -189,193 +68,113 @@ live_enable_disable_command_fn (vlib_main_t * vm,
                                    unformat_input_t * input,
                                    vlib_cli_command_t * cmd)
 {
-live_main_t * sm = &live_main;
-  ip6_address_t bsid;
+  live_main_t * lmp = &live_main;
+  u32 sw_if_index = ~0;
   int enable_disable = 1;
-  int b =0;
+
   int rv;
 
-  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT) {
-    if (unformat (input, "disable"))
-      enable_disable = 0;
-    else if (unformat (input, "bsid %U", unformat_ip6_address, &bsid))
-      b=1;
-    else
-      break;
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (input, "disable"))
+        enable_disable = 0;
+      else if (unformat (input, "%U", unformat_vnet_sw_interface,
+                         lmp->vnet_main, &sw_if_index))
+        ;
+      else
+        break;
   }
 
-  if (b==0)
-    return clib_error_return (0, "Please specify bsid of the policy");
-    
-  rv = live_enable_disable (sm, &bsid, enable_disable);
+  if (sw_if_index == ~0)
+    return clib_error_return (0, "Please specify an interface...");
 
-  switch(rv) {
+  rv = live_enable_disable (lmp, sw_if_index, enable_disable);
+
+  switch(rv)
+    {
   case 0:
     break;
 
-  case 1:
-    return clib_error_return (0, "No policies matched with the bsid");
-    break;  
+  case VNET_API_ERROR_INVALID_SW_IF_INDEX:
+    return clib_error_return
+      (0, "Invalid interface, only works on physical ports");
+    break;
+
+  case VNET_API_ERROR_UNIMPLEMENTED:
+    return clib_error_return (0, "Device driver doesn't support redirection");
+    break;
+
   default:
     return clib_error_return (0, "live_enable_disable returned %d",
                               rv);
-  }
+    }
   return 0;
 }
 
-
-VLIB_CLI_COMMAND (sr_content_command, static) = {
-    .path = "live sr policy",
-    .short_help = 
-    "live sr policy bsid <bsid> [disable]",
-    .function = live_enable_disable_command_fn,
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (live_enable_disable_command, static) =
+{
+  .path = "live enable-disable",
+  .short_help =
+  "live enable-disable <interface-name> [disable]",
+  .function = live_enable_disable_command_fn,
 };
+/* *INDENT-ON* */
 
-/**
- * @brief SRv6 Live-Live Plugin API message handler.
- */
+/* API message handler */
 static void vl_api_live_enable_disable_t_handler
 (vl_api_live_enable_disable_t * mp)
 {
   vl_api_live_enable_disable_reply_t * rmp;
-  live_main_t * sm = &live_main;
+  live_main_t * lmp = &live_main;
   int rv;
 
-  rv = live_enable_disable (sm, (ip6_address_t *) & mp->bsid_addr, 
+  rv = live_enable_disable (lmp, ntohl(mp->sw_if_index),
                                       (int) (mp->enable_disable));
-  
+
   REPLY_MACRO(VL_API_LIVE_ENABLE_DISABLE_REPLY);
 }
 
-/**
- * @brief Set up the API message handling tables.
- */
-static clib_error_t *
-live_plugin_api_hookup (vlib_main_t *vm)
-{
-  live_main_t * sm = &live_main;
-#define _(N,n)                                                  \
-    vl_msg_api_set_handlers((VL_API_##N + sm->msg_id_base),     \
-                           #n,					\
-                           vl_api_##n##_t_handler,              \
-                           vl_noop_handler,                     \
-                           vl_api_##n##_t_endian,               \
-                           vl_api_##n##_t_print,                \
-                           sizeof(vl_api_##n##_t), 1); 
-    foreach_live_plugin_api_msg;
-#undef _
+/* API definitions */
+#include <live/live.api.c>
 
-    return 0;
-}
-
-#define vl_msg_name_crc_list
-#include <live/live_all_api_h.h>
-#undef vl_msg_name_crc_list
-
-static void 
-setup_message_id_table (live_main_t * sm, api_main_t *am)
-{
-#define _(id,n,crc) \
-  vl_msg_api_add_msg_name_crc (am, #n "_" #crc, id + sm->msg_id_base);
-  foreach_vl_msg_name_crc_live;
-#undef _
-}
-
-/* Format for SLs DPO (same as defined in srv6 node) */
-static u8 *
-format_sr_live_segment_list_dpo (u8 * s, va_list * args)
-{
-  ip6_sr_main_t *sm = &sr_main;
-  ip6_address_t *addr;
-  ip6_sr_sl_t *sl;
-
-  index_t index = va_arg (*args, index_t);
-  CLIB_UNUSED (u32 indent) = va_arg (*args, u32);
-  s = format (s, "SR: Segment List index:[%d]", index);
-  s = format (s, "\n\tSegments:");
-
-  sl = pool_elt_at_index (sm->sid_lists, index);
-
-  s = format (s, "< ");
-  vec_foreach (addr, sl->segments)
-  {
-    s = format (s, "%U, ", format_ip6_address, addr);
-  }
-  s = format (s, "\b\b > - ");
-  s = format (s, "Weight: %u", sl->weight);
-
-  return s;
-}
-
-void
-sr_live_dpo_lock (dpo_id_t * dpo)
-{
-}
-
-
-void
-sr_live_dpo_unlock (dpo_id_t * dpo)
-{
-}
-
-const static dpo_vft_t sr_live_policy_rewrite_vft = {
-  .dv_lock = sr_live_dpo_lock,
-  .dv_unlock = sr_live_dpo_unlock,
-  .dv_format = format_sr_live_segment_list_dpo,
-};
-
-const static char *const sr_live_pr_encaps_ip6_nodes[] = {
-  "sr-live-pl-rewrite-encaps",
-  NULL,
-};
-
-
-const static char *const *const sr_live_pr_encaps_nodes[DPO_PROTO_NUM] = {
-  [DPO_PROTO_IP6] = sr_live_pr_encaps_ip6_nodes,
-};
-
-/**
- * @brief Initialize the SRv6 Live-Live plugin.
- */
 static clib_error_t * live_init (vlib_main_t * vm)
 {
-  live_main_t * sm = &live_main;
+  live_main_t * lmp = &live_main;
   clib_error_t * error = 0;
-  u8 * name;
 
-  vec_validate (sm->clones, vlib_num_workers());
-
-  mhash_init(&sm->flow_index_hash, sizeof(uword), sizeof(u32));
-
-  sr_live_pr_encaps_dpo_type =
-    dpo_register_new_type (&sr_live_policy_rewrite_vft, sr_live_pr_encaps_nodes);
-
-  sm->vnet_main =  vnet_get_main ();
-
-  name = format (0, "live_%08x%c", api_version, 0);
-
-  /* Ask for a correctly-sized block of API message decode slots */
-  sm->msg_id_base = vl_msg_api_get_msg_ids 
-      ((char *) name, VL_MSG_FIRST_AVAILABLE);
-
-  error = live_plugin_api_hookup (vm);
+  lmp->vlib_main = vm;
+  lmp->vnet_main = vnet_get_main();
 
   /* Add our API messages to the global name_crc hash table */
-  setup_message_id_table (sm, &api_main);
-
-  vec_free(name);
+  lmp->msg_id_base = setup_message_id_table ();
 
   return error;
 }
 
 VLIB_INIT_FUNCTION (live_init);
 
-/**
- * @brief Hook the Live-Live plugin into the VPP graph hierarchy.
- */
-VNET_FEATURE_INIT (sample, static) = 
+/* *INDENT-OFF* */
+VNET_FEATURE_INIT (live, static) =
 {
-  .arc_name = "ip6-unicast",
+  .arc_name = "device-input",
   .node_name = "live",
-  .runs_before =0,
+  .runs_before = VNET_FEATURES ("ethernet-input"),
 };
+/* *INDENT-ON */
+
+/* *INDENT-OFF* */
+VLIB_PLUGIN_REGISTER () =
+{
+  .version = VPP_BUILD_VER,
+  .description = "live plugin description goes here",
+};
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
--- live/live_test.c
+++ live/live_test.c
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2015 Cisco and/or its affiliates.
+ * live.c - live vpp-api-test plug-in
+ *
+ * Copyright (c) <current-year> <your-organization>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -12,166 +14,80 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/*
- *------------------------------------------------------------------
- * live_test.c - test live-live plugin
- *------------------------------------------------------------------
- */
-
 #include <vat/vat.h>
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>
 #include <vppinfra/error.h>
-#include <vnet/srv6/sr.h>
+#include <stdbool.h>
 
 #define __plugin_msg_base live_test_main.msg_id_base
 #include <vlibapi/vat_helper_macros.h>
 
-uword unformat_bsid (unformat_input_t * input, va_list * args);
+uword unformat_sw_if_index (unformat_input_t * input, va_list * args);
 
 /* Declare message IDs */
-#include <live/live_msg_enum.h>
+#include <live/live.api_enum.h>
+#include <live/live.api_types.h>
 
-/* define message structures */
-#define vl_typedefs
-#include <live/live_all_api_h.h> 
-#undef vl_typedefs
-
-/* declare message handlers for each api */
-
-#define vl_endianfun             /* define message structures */
-#include <live/live_all_api_h.h> 
-#undef vl_endianfun
-
-/* instantiate all the print functions we know about */
-#define vl_print(handle, ...)
-#define vl_printfun
-#include <live/live_all_api_h.h> 
-#undef vl_printfun
-
-/* Get the API version number. */
-#define vl_api_version(n,v) static u32 api_version=(v);
-#include <live/live_all_api_h.h>
-#undef vl_api_version
-
-
-typedef struct {
-    /* API message ID base */
-    u16 msg_id_base;
-    vat_main_t *vat_main;
+typedef struct
+{
+  /* API message ID base */
+  u16 msg_id_base;
+  vat_main_t *vat_main;
 } live_test_main_t;
 
 live_test_main_t live_test_main;
 
-#define foreach_standard_reply_retval_handler   \
-_(live_enable_disable_reply)
-
-#define _(n)                                            \
-    static void vl_api_##n##_t_handler                  \
-    (vl_api_##n##_t * mp)                               \
-    {                                                   \
-        vat_main_t * vam = live_test_main.vat_main;   \
-        i32 retval = ntohl(mp->retval);                 \
-        if (vam->async_mode) {                          \
-            vam->async_errors += (retval < 0);          \
-        } else {                                        \
-            vam->retval = retval;                       \
-            vam->result_ready = 1;                      \
-        }                                               \
-    }
-foreach_standard_reply_retval_handler;
-#undef _
-
-/* 
- * Table of message reply handlers, must include boilerplate handlers
- * we just generated
- */
-#define foreach_vpe_api_reply_msg                                       \
-_(LIVE_ENABLE_DISABLE_REPLY, live_enable_disable_reply)
-
-
 static int api_live_enable_disable (vat_main_t * vam)
 {
-    unformat_input_t * i = vam->input;
-    int enable_disable = 1;
-    ip6_address_t bsid;
-    vl_api_live_enable_disable_t * mp;
-    int ret;
+  unformat_input_t * i = vam->input;
+  int enable_disable = 1;
+  u32 sw_if_index = ~0;
+  vl_api_live_enable_disable_t * mp;
+  int ret;
 
-    /* Parse args required to build the message */
-    while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT) {
-   /*     if (unformat (i, "%U", unformat_bsid, vam, &bsid))
-            ;
-	else*/ if (unformat (i, "bsid %d", &bsid))
-	    ;
-        else if (unformat (i, "disable"))
-            enable_disable = 0;
-        else
-            break;
+  /* Parse args required to build the message */
+  while (unformat_check_input (i) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (i, "%U", unformat_sw_if_index, vam, &sw_if_index))
+          ;
+        else if (unformat (i, "sw_if_index %d", &sw_if_index))
+          ;
+      else if (unformat (i, "disable"))
+          enable_disable = 0;
+      else
+          break;
     }
-    
-    
-    
-    /* Construct the API message */
-    M(LIVE_ENABLE_DISABLE, mp);
-    clib_memcpy(mp->bsid_addr, &bsid, sizeof(ip6_address_t));
-    mp->enable_disable = enable_disable;
 
-    /* send it... */
-    S(mp);
+  if (sw_if_index == ~0)
+    {
+      errmsg ("missing interface name / explicit sw_if_index number \n");
+      return -99;
+    }
 
-    /* Wait for a reply... */
-    W (ret);
-    return ret;
+  /* Construct the API message */
+  M(LIVE_ENABLE_DISABLE, mp);
+  mp->sw_if_index = ntohl (sw_if_index);
+  mp->enable_disable = enable_disable;
+
+  /* send it... */
+  S(mp);
+
+  /* Wait for a reply... */
+  W (ret);
+  return ret;
 }
 
-/* 
- * List of messages that the api test plugin sends,
+/*
+ * List of messages that the live test plugin sends,
  * and that the data plane plugin processes
  */
-#define foreach_vpe_api_msg \
-_(live_enable_disable, "<bsid> [disable]")
+#include <live/live.api_test.c>
 
-static void live_api_hookup (vat_main_t *vam)
-{
-    live_test_main_t * sm = &live_test_main;
-    /* Hook up handlers for replies from the data plane plug-in */
-#define _(N,n)                                                  \
-    vl_msg_api_set_handlers((VL_API_##N + sm->msg_id_base),     \
-                           #n,                                  \
-                           vl_api_##n##_t_handler,              \
-                           vl_noop_handler,                     \
-                           vl_api_##n##_t_endian,               \
-                           vl_api_##n##_t_print,                \
-                           sizeof(vl_api_##n##_t), 1); 
-    foreach_vpe_api_reply_msg;
-#undef _
-
-    /* API messages we can send */
-#define _(n,h) hash_set_mem (vam->function_by_name, #n, api_##n);
-    foreach_vpe_api_msg;
-#undef _    
-    
-    /* Help strings */
-#define _(n,h) hash_set_mem (vam->help_by_name, #n, h);
-    foreach_vpe_api_msg;
-#undef _
-}
-
-clib_error_t * vat_plugin_register (vat_main_t *vam)
-{
-  live_test_main_t * sm = &live_test_main;
-  u8 * name;
-
-  sm->vat_main = vam;
-
-  name = format (0, "live_%08x%c", api_version, 0);
-  sm->msg_id_base = vl_client_get_first_plugin_msg_id ((char *) name);
-
-  if (sm->msg_id_base != (u16) ~0)
-    live_api_hookup (vam);
-  
-  vec_free(name);
-  
-  return 0;
-}
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */
