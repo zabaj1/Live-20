d0712782e4da86a2f17472b25910032cb486ede9
Marco <mi4998@mclink.it>
2020-09-08 14:26:21 +0200

    improved info in file description

--- live/live.h
+++ live/live.h
@@ -1,8 +1,5 @@
-
 /*
- * live.h - skeleton vpp engine plug-in header file
- *
- * Copyright (c) <current-year> <your-organization>
+ * Copyright (c) 2015 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
